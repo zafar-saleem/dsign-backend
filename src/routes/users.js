149675b1547b97bd89a6f8e2035ccb7ff1a95f80
Zafar Saleem <zafarsaleem3@gmail.com>
2017-03-02 06:35:33 -0500

    received login credentials on backend

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,6 +12,7 @@ router.post('/register', (request, response) => {
     let repeatPassword = request.body.repeatPassword;
 
     request.checkBody('email', 'Email is required').notEmpty();
+    request.checkBody('email', 'Please provide valid email address').isEmail();
     request.checkBody('password', 'Password is required').notEmpty();
     request.checkBody('repeatPassword', 'Repeat password is required').notEmpty();
 
@@ -25,6 +26,20 @@ router.post('/register', (request, response) => {
 
 router.get('/login', (request, response) => {
     response.render('login');
+});
+
+router.post('/login', (request, response) => {
+    let email = request.body.email;
+    let password = request.body.password;
+
+    request.checkBody('email', 'Please provide an email address').notEmpty();
+    request.checkBody('email', 'Please provide valid email address').isEmail();
+    request.checkBody('password', 'Please provide password').notEmpty();
+
+    let errors = request.validationErrors();
+
+    response.json(errors);
+    // console.log(request.body);
 });
 
 module.exports = router;
